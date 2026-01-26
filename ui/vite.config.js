1817932a6da326484d7faf3afebc6d0f8cea7b23
EPIS <episprincipal@gmail.com>
2026-01-25 23:58:23 -0600

    CORS error

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig( {
   plugins: [ react(), tailwindcss() ],
-  base: "./",
+  base: "/",
   server: {
     host: true,
     port: 8081,
